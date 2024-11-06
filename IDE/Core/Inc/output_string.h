2a872c1b28cbdb92490c12ee56228907b1ea929d
FanMinkJonk <127284415+FanMinkJonk@users.noreply.github.com>
2024-11-06 15:21:55 +0700

    Add and test i2c-lcd

---
+++ IDE/Core/Inc/output_string.h
@@ -0,0 +1,17 @@
+/*
+ * output_string.h
+ *
+ *  Created on: Nov 6, 2024
+ *      Author: ACER
+ */
+
+#ifndef INC_OUTPUT_STRING_H_
+#define INC_OUTPUT_STRING_H_
+
+#include "main.h"
+#include "string.h"
+
+extern char str1[16];
+extern char str2[16];
+
+#endif /* INC_OUTPUT_STRING_H_ */
---
+++ IDE/Core/Src/output_string.c
@@ -0,0 +1,11 @@
+/*
+ * output_string.c
+ *
+ *  Created on: Nov 6, 2024
+ *      Author: ACER
+ */
+
+#include "output_string.h"
+
+char str1[16] = "Xin moi quet the";
+char str2[16] = "................";
